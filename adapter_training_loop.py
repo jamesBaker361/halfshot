e739ac2e1e73b024de7e2bbcaba6e4e24abdf0d6
James Baker <jlb638@amarel2.amarel.rutgers.edu>
2024-02-23 22:38:55 -0500

    loop for ip adapter?

--- adapter_training_loop.py
+++ adapter_training_loop.py
@@ -1,37 +1,134 @@
-from diffusers import AutoPipelineForText2Image, StableDiffusionPipeline
+import os
+from diffusers import AutoencoderKL, DDPMScheduler, DiffusionPipeline, StableDiffusionPipeline, UNet2DConditionModel
 from PIL import Image
+from peft.utils import get_peft_model_state_dict
+from string_globals import *
 from torch.utils.data import DataLoader
 from torchvision import transforms
 from data_helpers import make_dataloader
+import wandb
+import torch
+import torch.nn.functional as F
 
-def train_loop(images: list,
+def loop(images: list,
                text_prompt:str,
-               src_image: Image,
+               ip_adapter_image: Image,
                pipeline:StableDiffusionPipeline,
+               timesteps_per_image:int,
+               start_epoch:int,
                epochs:int,
                optimizer:object,
                lr_scheduler: object,
                accelerator:object,
-               size:int,train_batch_size:int
+               size:int,train_batch_size:int,
+               output_dir:str,
+               use_ip_adapter:bool,
+               with_prior_preservation:bool,
+               num_validation_images:int,
+               seed:int=0,
+               noise_offset:float=None,
+               max_grad_norm:float=1.0
                )->StableDiffusionPipeline:
     '''
-    given images generated from text prompt, and the src_image, trains the pipeline for epochs
+    given images generated from text prompt, and the src_image, trains the unet lora pipeline for epochs
     using the prompt and the src_image for conditioning and returns the trained pipeline
     
     images: PIL imgs to be used for training
+    ip_adapter_image: 
     text_prompt: text prompt describing character
     pipeline: should already have ip adapter loaded
+    start_epoch: epoch we're starting at
     epochs: how many epochs to do this training
     optimizer: optimizer
     acceelrator: accelerator object
     size: img dim 
     train_batch_size: batch size
+    with_prior_preservation: whether to use prior preservation (for dreambooth)
+    noise_offset: https://www.crosslabs.org//blog/diffusion-with-offset-noise
     '''
     tokenizer=pipeline.tokenizer
+    vae=pipeline.vae
+    text_encoder=pipeline.text_encoder
     dataloader=make_dataloader(images,text_prompt,tokenizer,size, train_batch_size)
     unet=tokenizer.unet
-    #lora_layers = filter(lambda p: p.requires_grad, unet.parameters()) optimizer should already be listening to lora_layers
+    lora_layers = filter(lambda p: p.requires_grad, unet.parameters()) #optimizer should already be listening to whatever layers we're optimzing
     unet, optimizer, dataloader, lr_scheduler = accelerator.prepare(
         unet, optimizer, dataloader, lr_scheduler
     )
+    added_cond_kwargs={}
+    if use_ip_adapter:
+        image_embeds = pipeline.prepare_ip_adapter_image_embeds(
+                    ip_adapter_image, None, accelerator.device, train_batch_size)
+        added_cond_kwargs = {"image_embeds": image_embeds}
+    weight_dtype=pipeline.dtype
+    noise_scheduler = DDPMScheduler(num_train_timesteps=timesteps_per_image,clip_sample=False)
+    for e in range(start_epoch, epochs):
+        unet.train()
+        train_loss = 0.0
+        global_step=0
+        for step,batch in enumerate(dataloader):
+            with accelerator.accumulate(unet):
+                latents = vae.encode(batch[IMAGES].to(dtype=weight_dtype)).latent_dist.sample()
+                latents = latents * vae.config.scaling_factor
+
+                # Sample noise that we'll add to the latents
+                noise = torch.randn_like(latents)
+                if noise_offset:
+                    noise += noise_offset * torch.randn(
+                        (latents.shape[0], latents.shape[1], 1, 1), device=latents.device
+                    )
+                bsz = latents.shape[0]
+                # Sample a random timestep for each image
+                timesteps = torch.randint(0, noise_scheduler.config.num_train_timesteps, (bsz,), device=latents.device)
+                timesteps = timesteps.long()
+                # Add noise to the latents according to the noise magnitude at each timestep
+                # (this is the forward diffusion process)
+                noisy_latents = noise_scheduler.add_noise(latents, noise, timesteps)
+
+                # Get the text embedding for conditioning
+                encoder_hidden_states = text_encoder(batch[TEXT_INPUT_IDS])[0]
+
+                noise_pred = unet(noisy_latents, 
+                                timesteps, 
+                                encoder_hidden_states,
+                                added_cond_kwargs=added_cond_kwargs).sample
+                
+                loss = F.mse_loss(noise_pred.float(), noise.float(), reduction="mean")
+
+                avg_loss = accelerator.gather(loss.repeat(train_batch_size)).mean()
+                train_loss += avg_loss.item()
+                # Backpropagate
+                accelerator.backward(loss)
+                if accelerator.sync_gradients:
+                    params_to_clip = lora_layers
+                    accelerator.clip_grad_norm_(params_to_clip, max_grad_norm)
+                optimizer.step()
+                lr_scheduler.step()
+                optimizer.zero_grad()
+            if accelerator.sync_gradients:
+                global_step += 1
+                accelerator.log({"train_loss": train_loss}, step=global_step)
+                train_loss = 0.0
+        if accelerator.is_main_process:
+            save_path = os.path.join(output_dir, f"checkpoint-{e}")
+            accelerator.save_state(save_path)
+
+            unet_lora_state_dict = get_peft_model_state_dict(unet)
+
+            StableDiffusionPipeline.save_lora_weights(
+                save_directory=save_path,
+                unet_lora_layers=unet_lora_state_dict,
+                safe_serialization=True,
+            )
+
+            generator = torch.Generator(device=accelerator.device)
+            generator.set_manual_seed(seed)
+
+            tracker=accelerator.get_tracker("wandb")
+            path="tmp.png"
+            for i in range(num_validation_images):
+                img=pipeline(text_prompt, num_inference_steps=timesteps_per_image, generator=generator).images[0]
+                img.save(path)
+                tracker.log({f"img_{i}": wandb.Image(path)},step=e)
+
     return pipeline
