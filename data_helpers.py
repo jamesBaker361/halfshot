3581bb8d8573240cd4e61e6138f630313e57e8e7
James Baker <jlb638@amarel2.amarel.rutgers.edu>
2024-02-24 11:56:43 -0500

    nit

--- data_helpers.py
+++ data_helpers.py
@@ -22,7 +22,7 @@ def make_dataloader(images: list, text_prompt_list:str,tokenizer:object,size:int
         IMAGES:[] #images used for latents (lora trainign script calls it pixel values)
     }
     clip_image_processor = CLIPImageProcessor()
-    for image,text_prompot in zip(images,text_prompt_list):
+    for image,text_prompt in zip(images,text_prompt_list):
         text_input_ids=tokenizer(
             text_prompt,max_length=tokenizer.model_max_length, padding="max_length", truncation=True, return_tensors="pt"
         ).input_ids
