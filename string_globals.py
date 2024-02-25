3861d4993fda950eef769773423d9e79ce24fc80
James Baker <jlb638@amarel1.amarel.rutgers.edu>
2024-02-25 12:00:40 -0500

    new globals for prior_preservation

--- string_globals.py
+++ string_globals.py
@@ -1,3 +1,5 @@
 TEXT_INPUT_IDS="text_input_ids"
 CLIP_IMAGES='clip_images'
 IMAGES="images" #in text_to_image_lora this is aka pixel_values
+PRIOR_IMAGES="prior_images"
+PRIOR_TEXT_INPUT_IDS="prior_text_input_ids"
