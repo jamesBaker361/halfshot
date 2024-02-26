2539074b326d0a7bc3fbc8d92e3aeda915dc4d26
James Baker <jlb638@amarel1.amarel.rutgers.edu>
2024-02-25 21:35:26 -0500

    new token

--- string_globals.py
+++ string_globals.py
@@ -3,3 +3,4 @@ CLIP_IMAGES='clip_images'
 IMAGES="images" #in text_to_image_lora this is aka pixel_values
 PRIOR_IMAGES="prior_images"
 PRIOR_TEXT_INPUT_IDS="prior_text_input_ids"
+NEW_TOKEN="<xyz>" #this is the new token we will use for dreambooth/textual inversion
