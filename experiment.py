b5b5172bb9fc22265260e6590f633c5d4e717801
James Baker <jlbaker361@gmail.com>
2024-03-21 15:11:38 -0400

    cleanup

--- experiment.py
+++ experiment.py
@@ -334,11 +334,11 @@ def train_and_evaluate(ip_adapter_image:Image,
         text_prompt_list=[NEW_TOKEN]*n_image
         entity_name=NEW_TOKEN
         validation_prompt_list=text_prompt_list
-    if training_method in [DB,DB_MULTI]:
+    if training_method.find(DB_MULTI)!=-1 and training_method.find(IP)==-1:
         prior_images=[
             pipeline(description_prompt,negative_prompt=cold_prompt,safety_checker=None, num_inference_steps=timesteps_per_image).images[0] for _ in range(n_image)
         ]
-    if training_method in [DB_MULTI_IP]:
+    if training_method.find(DB_MULTI)!=-1 and training_method.find(IP)!=-1:
         prior_images=[
             pipeline(description_prompt,negative_prompt=cold_prompt,safety_checker=None,ip_adapter_image=ip_adapter_image, num_inference_steps=timesteps_per_image).images[0] for _ in range(n_image)
         ]
